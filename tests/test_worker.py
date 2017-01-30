7fd61a93d204b720042f76480d268e461a80c62a
Thomas Schraitle <toms@suse.de>
2017-01-30 11:17:04 +0100

    Improve test cases for docstats.worker.clone_repo

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,7 +1,38 @@
 import pytest
+import os
+import shutil
 
+from unittest.mock import patch, Mock
 from docstats.worker import clone_repo
+import git
 
-@pytest.mark.skip
-def test_clone_repo():
-    pass
+USER = os.environ["USER"]
+PYTESTTMPDIR = "/tmp/pytest-of-%s/" % USER
+SECTION = "git-test-repo"
+GITDIR =  os.path.join(PYTESTTMPDIR, SECTION)
+GITREPO = None
+
+
+@pytest.fixture(scope="module",)
+def gitrepo():
+    global GITREPO, GITDIR
+    if GITREPO is None:
+        GITREPO = git.Repo.init(GITDIR, mkdir=True)
+    yield GITREPO
+    GITREPO = None
+    shutil.rmtree(GITDIR)
+
+
+
+def test_clone_repo_if_path_exists(gitrepo):
+    repo = clone_repo(SECTION, 'fake-url', PYTESTTMPDIR)
+    assert repo.git_dir == GITREPO.git_dir
+
+
+@patch('docstats.worker.os.path.exists')
+@patch('docstats.worker.git.Repo')
+def test_clone_repo(mock_repo, mock_path):
+    mock_repo.clone_from = Mock(return_value=GITREPO)
+    mock_path.return_value = False
+    repo = clone_repo(SECTION, 'fake-url', PYTESTTMPDIR)
+    assert repo.git_dir == GITREPO.git_dir
