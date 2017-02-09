9b1df4a1b48592ccba1cc7bc51659d871699ca6d
Thomas Schraitle <toms@suse.de>
2017-02-09 09:43:13 +0100

    Skip test when Python version < 3.4.5

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -2,7 +2,15 @@
 
 import pytest
 import py
+import sys
 
 
+skip_below_py345 = pytest.mark.skipif((sys.version_info.major,
+                                       sys.version_info.minor,
+                                       sys.version_info.micro) < (3, 4, 5),
+                               reason="skip test for python3.4.x")
+
+
+@skip_below_py345
 def test_repo(git_repo):
     print("Git-Repo:", git_repo)
